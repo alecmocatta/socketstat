68f1842fdefa852cc077a4cbe4c20dc9eaa915be
alecmocatta <alec@mocatta.net>
2019-08-22 20:08:24 +0100

    initial

--- README.md
+++ README.md
@@ -1,26 +1,101 @@
-# template-rust
+# socketstat
 
-[![Crates.io](https://img.shields.io/crates/v/template-rust.svg?maxAge=86400)](https://crates.io/crates/template-rust)
-[![MIT / Apache 2.0 licensed](https://img.shields.io/crates/l/template-rust.svg?maxAge=2592000)](#License)
-[![Build Status](https://dev.azure.com/alecmocatta/template-rust/_apis/build/status/tests?branchName=master)](https://dev.azure.com/alecmocatta/template-rust/_build/latest?branchName=master)
+[![Crates.io](https://img.shields.io/crates/v/socketstat.svg?maxAge=86400)](https://crates.io/crates/socketstat)
+[![MIT / Apache 2.0 licensed](https://img.shields.io/crates/l/socketstat.svg?maxAge=2592000)](#License)
+[![Build Status](https://dev.azure.com/alecmocatta/socketstat/_apis/build/status/tests?branchName=master)](https://dev.azure.com/alecmocatta/socketstat/_build/latest?branchName=master)
 
-[Docs](https://docs.rs/template-rust/0.1.0)
+[Docs](https://docs.rs/socketstat/0.1.0)
 
-A template Rust library crate.
+Get socket information and statistics.
 
-This is template for Rust libraries, comprising a [`hello_world()`](https://docs.rs/template-rust/0.1.0/template_rust/fn.hello_world.html) function.
+Currently works on macOS only, PRs for other platforms welcome!
 
 ## Example
 
 ```rust
-use template_rust::hello_world;
+#[cfg(unix)]
+use std::os::unix::io::AsRawFd;
+#[cfg(windows)]
+use std::os::windows::io::AsRawSocket;
+use socketstat::socketstat;
 
-hello_world();
+let sock = std::net::TcpStream::connect("google.com:80").unwrap();
+
+#[cfg(unix)]
+let fd = sock.as_raw_fd();
+#[cfg(windows)]
+let fd = sock.as_raw_socket();
+
+println!("{:#?}", socketstat(fd));
+
+// prints:
+//   Ok(
+//       SocketStat {
+//           unreceived: 0,
+//           unsent: 0,
+//           connection_info: tcp_connection_info {
+//               tcpi_state: "ESTABLISHED",
+//               tcpi_snd_wscale: 8,
+//               tcpi_rcv_wscale: 6,
+//               tcpi_options: 7,
+//               tcpi_flags: 0,
+//               tcpi_rto: 0,
+//               tcpi_maxseg: 1368,
+//               tcpi_snd_ssthresh: 1073725440,
+//               tcpi_snd_cwnd: 4380,
+//               tcpi_snd_wnd: 60192,
+//               tcpi_snd_sbbytes: 0,
+//               tcpi_rcv_wnd: 131328,
+//               tcpi_rttcur: 79,
+//               tcpi_srtt: 79,
+//               tcpi_rttvar: 39,
+//               tcpi_tfo: 0,
+//               tcpi_txpackets: 0,
+//               tcpi_txbytes: 0,
+//               tcpi_txretransmitbytes: 0,
+//               tcpi_rxpackets: 0,
+//               tcpi_rxbytes: 0,
+//               tcpi_rxoutoforderbytes: 0,
+//               tcpi_txretransmitpackets: 0,
+//           },
+//           socket_info: tcp_sockinfo {
+//               tcpsi_ini: in_sockinfo {
+//                   insi_fport: 80,
+//                   insi_lport: 52621,
+//                   insi_gencnt: 100950561,
+//                   insi_flags: 8390720,
+//                   insi_flow: 0,
+//                   insi_vflag: "IPV4",
+//                   insi_ip_ttl: 64,
+//                   rfu_1: 0,
+//               },
+//               tcpsi_state: "ESTABLISHED",
+//               tcpsi_timer: [
+//                   0,
+//                   0,
+//                   7200079,
+//                   0,
+//               ],
+//               tcpsi_mss: 1368,
+//               tcpsi_flags: 1140851680,
+//               rfu_1: 0,
+//               tcpsi_tp: 9662996336038732135,
+//           },
+//       },
+//   )
 ```
 
 ## Note
 
-Caveat emptor.
+On macOS this calls:
+* `getsockopt(fd, IPPROTO_TCP, TCP_CONNECTION_INFO, ...)`
+* `proc_pidfdinfo(getpid(), fd, PROC_PIDFDSOCKETINFO, ...)`
+* `ioctl(fd, FIONREAD, ...)`
+* `getsockopt(fd, SOL_SOCKET, SO_NWRITE, ...)`
+
+Other sources to explore:
+* `sysctl([CTL_NET, PF_INET, IPPROTO_TCP, TCPCTL_PCBLIST], ...`
+* <https://stackoverflow.com/questions/31263289/on-linux-mac-windows-is-it-possible-to-access-the-tcp-timestamp-and-or-rtt-in-u>
 
 ## License
 Licensed under either of
--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,97 @@
-//! A template Rust library crate.
+//! Get socket information and statistics.
 //!
-//! **[Crates.io](https://crates.io/crates/template-rust) │ [Repo](https://github.com/alecmocatta/template-rust)**
+//! **[Crates.io](https://crates.io/crates/socketstat) │ [Repo](https://github.com/alecmocatta/socketstat)**
 //!
-//! This is template for Rust libraries, comprising a [`hello_world()`] function.
+//! Currently works on macOS only, PRs for other platforms welcome!
 //!
 //! # Example
 //!
 //! ```
-//! use template_rust::hello_world;
+//! #[cfg(unix)]
+//! use std::os::unix::io::AsRawFd;
+//! #[cfg(windows)]
+//! use std::os::windows::io::AsRawSocket;
+//! use socketstat::socketstat;
 //!
-//! hello_world();
-//! // prints: Hello, world!
+//! let sock = std::net::TcpStream::connect("google.com:80").unwrap();
+//!
+//! #[cfg(unix)]
+//! let fd = sock.as_raw_fd();
+//! #[cfg(windows)]
+//! let fd = sock.as_raw_socket();
+//!
+//! println!("{:#?}", socketstat(fd));
+//!
+//! // prints:
+//! //   Ok(
+//! //       SocketStat {
+//! //           unreceived: 0,
+//! //           unsent: 0,
+//! //           connection_info: tcp_connection_info {
+//! //               tcpi_state: "ESTABLISHED",
+//! //               tcpi_snd_wscale: 8,
+//! //               tcpi_rcv_wscale: 6,
+//! //               tcpi_options: 7,
+//! //               tcpi_flags: 0,
+//! //               tcpi_rto: 0,
+//! //               tcpi_maxseg: 1368,
+//! //               tcpi_snd_ssthresh: 1073725440,
+//! //               tcpi_snd_cwnd: 4380,
+//! //               tcpi_snd_wnd: 60192,
+//! //               tcpi_snd_sbbytes: 0,
+//! //               tcpi_rcv_wnd: 131328,
+//! //               tcpi_rttcur: 79,
+//! //               tcpi_srtt: 79,
+//! //               tcpi_rttvar: 39,
+//! //               tcpi_tfo: 0,
+//! //               tcpi_txpackets: 0,
+//! //               tcpi_txbytes: 0,
+//! //               tcpi_txretransmitbytes: 0,
+//! //               tcpi_rxpackets: 0,
+//! //               tcpi_rxbytes: 0,
+//! //               tcpi_rxoutoforderbytes: 0,
+//! //               tcpi_txretransmitpackets: 0,
+//! //           },
+//! //           socket_info: tcp_sockinfo {
+//! //               tcpsi_ini: in_sockinfo {
+//! //                   insi_fport: 80,
+//! //                   insi_lport: 52621,
+//! //                   insi_gencnt: 100950561,
+//! //                   insi_flags: 8390720,
+//! //                   insi_flow: 0,
+//! //                   insi_vflag: "IPV4",
+//! //                   insi_ip_ttl: 64,
+//! //                   rfu_1: 0,
+//! //               },
+//! //               tcpsi_state: "ESTABLISHED",
+//! //               tcpsi_timer: [
+//! //                   0,
+//! //                   0,
+//! //                   7200079,
+//! //                   0,
+//! //               ],
+//! //               tcpsi_mss: 1368,
+//! //               tcpsi_flags: 1140851680,
+//! //               rfu_1: 0,
+//! //               tcpsi_tp: 9662996336038732135,
+//! //           },
+//! //       },
+//! //   )
 //! ```
 //!
 //! # Note
 //!
-//! Caveat emptor.
+//! On macOS this calls:
+//! * `getsockopt(fd, IPPROTO_TCP, TCP_CONNECTION_INFO, ...)`
+//! * `proc_pidfdinfo(getpid(), fd, PROC_PIDFDSOCKETINFO, ...)`
+//! * `ioctl(fd, FIONREAD, ...)`
+//! * `getsockopt(fd, SOL_SOCKET, SO_NWRITE, ...)`
+//!
+//! Other sources to explore:
+//! * `sysctl([CTL_NET, PF_INET, IPPROTO_TCP, TCPCTL_PCBLIST], ...`
+//! * <https://stackoverflow.com/questions/31263289/on-linux-mac-windows-is-it-possible-to-access-the-tcp-timestamp-and-or-rtt-in-u>
 
-#![doc(html_root_url = "https://docs.rs/template-rust/0.1.0")]
+#![doc(html_root_url = "https://docs.rs/socketstat/0.1.0")]
 #![warn(
 	missing_copy_implementations,
 	missing_debug_implementations,
@@ -31,30 +105,70 @@
 )] // from https://github.com/rust-unofficial/patterns/blob/master/anti_patterns/deny-warnings.md
 #![allow()]
 
-/// Print "Hello, world!".
-///
-/// # Example
-///
-/// ```
-/// use template_rust::hello_world;
-///
-/// hello_world();
-/// // prints: Hello, world!
-/// ```
-///
-/// # Panics
-///
-/// Will panic if printing fails.
-pub fn hello_world() {
-	print!("Hello, world!");
+use std::fmt;
+
+#[cfg(unix)]
+type Fd = std::os::unix::io::RawFd;
+#[cfg(windows)]
+type Fd = std::os::windows::io::RawSocket;
+
+#[cfg(any(target_os = "macos", target_os = "ios"))]
+mod mac;
+#[cfg(any(target_os = "macos", target_os = "ios"))]
+use mac as sys;
+
+#[cfg(not(any(target_os = "macos", target_os = "ios")))]
+mod stub;
+#[cfg(not(any(target_os = "macos", target_os = "ios")))]
+use stub as sys;
+
+/// Information and stats that can be printed via the Debug implementation.
+#[derive(Clone)]
+pub struct SocketStat {
+	sys: sys::SocketStat,
+}
+impl fmt::Debug for SocketStat {
+	fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+		self.sys.fmt(fmt)
+	}
+}
+
+/// An error occurred
+#[derive(Clone)]
+pub struct Error {
+	sys: sys::Error,
+}
+impl fmt::Debug for Error {
+	fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+		self.sys.fmt(fmt)
+	}
+}
+
+/// Get socket information and statistics for a given File Descriptor or Socket.
+pub fn socketstat(fd: Fd) -> Result<SocketStat, Error> {
+	sys::socketstat(fd)
+		.map(|sys| SocketStat { sys })
+		.map_err(|sys| Error { sys })
 }
 
 #[cfg(test)]
 mod tests {
-	use super::hello_world;
+	#[cfg(unix)]
+	use std::os::unix::io::AsRawFd;
+	#[cfg(windows)]
+	use std::os::windows::io::AsRawSocket;
+
+	use super::socketstat;
 
 	#[test]
 	fn succeeds() {
-		hello_world();
+		let sock = std::net::TcpStream::connect("google.com:80").unwrap();
+
+		#[cfg(unix)]
+		let fd = sock.as_raw_fd();
+		#[cfg(windows)]
+		let fd = sock.as_raw_socket();
+
+		println!("{:#?}", socketstat(fd));
 	}
 }
